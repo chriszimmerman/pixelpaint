60c996bbc43cacfca397f06998f00b43729f120e
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2015-04-09 22:11:57 -0400

    allow user to change size of canvas

--- pixelpaint.js
+++ pixelpaint.js
@@ -1,20 +1,37 @@
-//jquery 1.9.1
+var tableWidth;
+var tableHeight;
+
 $(document).ready(function(){
-    var tableSize = 10;
-    
-    for(var i = 0; i < tableSize; i++){
-        var tableRowId = 'row' + i;
-        
-        $('#canvas').append('<tr id="' + tableRowId + '"></tr>"');
-        
-        for(var j = 0; j < tableSize; j++){
-            $('#' + tableRowId).append('<td><div class="square"></div></td>');            
-        }
-        
-    }
-    
-    $('.square').click(function(){
-        var color = $('#colorpicker').val();
-         $(this).css('backgroundColor', color);
-    });
+    tableWidth = 10;
+    tableHeight = 10;
+
+	function createTable(){
+		tableWidth = $('#width').val();
+		tableHeight = $('#height').val();
+
+		for(var i = 0; i < tableHeight; i++){
+			var tableRowId = 'row' + i;
+			
+			$('#canvas').append('<tr id="' + tableRowId + '"></tr>"');
+			
+			for(var j = 0; j < tableWidth; j++){
+				$('#' + tableRowId).append('<td><div class="square"></div></td>');            
+			}
+		}
+		
+		$('.square').click(function(){
+			var color = $('#colorpicker').val();
+			$(this).css('backgroundColor', color);
+		});
+	}
+
+    $('#width').val(tableWidth);
+    $('#height').val(tableHeight);
+
+    $('#update').click(function(){
+		$('#canvas').empty();
+		createTable();
+	});
+
+	createTable();
 });
