1a0ff1e30b380e7fc1246a3db5e0be83bdff81f3
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2015-04-24 23:01:30 -0400

    wrote test for flipImage()

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,14 @@
-QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
+QUnit.test("flipImage", function(assert) {
+	var upsideDownImageData = ['1', '0', '0', '1'];
+	var height = 2;
+	var width = 2;
+	var upsideDownImage = new image(width, height);
+	upsideDownImage.data = upsideDownImageData;
+
+	flipImage(upsideDownImage);
+
+	var expectedImageData = ['0', '1', '1', '0'];
+	var actual = upsideDownImage.data;
+
+	assert.deepEqual(actual, expectedImageData);
 });
